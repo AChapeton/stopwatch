35907e66aade3ddf26b33d73cf1db2e0b58e2f72
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-02-23 23:09:23 -0600

    Create counters for minutes and hours

--- src/modules/Stopwatch/index.js
+++ src/modules/Stopwatch/index.js
@@ -2,19 +2,32 @@ import { useState, useEffect, useRef } from "react";
 
 const Stopwatch = () => {
   const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
   const [isActive, setIsActive] = useState(false);
   const [buttonText, setButtonText] = useState("Start");
 
-  const intervalRef = useRef();
+  const secondsRef = useRef();
+  const minutesRef = useRef();
+  const hoursRef = useRef();
 
   useEffect(() => {
-    intervalRef.current = setInterval(() => {
+    secondsRef.current = setInterval(() => {
       if (isActive) {
         setSeconds((current) => current + 1);
+        if (seconds >= 59) {
+          setSeconds((current) => (current = 0));
+          setMinutes((current) => current + 1);
+          if (minutes >= 59) {
+            setSeconds((current) => (current = 0));
+            setMinutes((current) => (current = 0));
+            setHours((current) => current + 1);
+          }
+        }
       }
-    }, 1000);
-    return () => clearInterval(intervalRef.current);
-  }, [isActive]);
+    }, 50);
+    return () => clearInterval(secondsRef.current);
+  }, [isActive, seconds, minutes, hours]);
 
   const onStartCount = () => {
     if (isActive) {
@@ -25,13 +38,21 @@ const Stopwatch = () => {
     setIsActive(!isActive);
   };
 
+  const onRestartCount = () => {
+    setIsActive(false);
+    onStartCount();
+    setSeconds(0);
+    setMinutes(0);
+    setHours(0);
+  };
+
   return (
     <div>
-      <p ref={intervalRef}>Seconds: {seconds}</p>
-      <span>00:</span>
-      <span>00:</span>
-      <span>00</span>
+      <span ref={hoursRef}>{hours < 10 ? `0${hours}` : hours}:</span>
+      <span ref={minutesRef}>{minutes < 10 ? `0${minutes}` : minutes}:</span>
+      <span ref={secondsRef}>{seconds < 10 ? `0${seconds}` : seconds}</span>
       <button onClick={onStartCount}>{buttonText}</button>
+      <button onClick={onRestartCount}>Restart</button>
     </div>
   );
 };
